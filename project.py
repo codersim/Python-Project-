925093b24c57fd7fb0c0d961b078fe357b060c60
bhags97 <bhagya28@hotmail.co.uk>
2020-04-24 08:22:13 +0100

    project.py

--- project.py
+++ project.py
@@ -1,4 +1,36 @@
-print("Hello there!")
-print("New feature")
+# 1. Read the data from the spreadsheet
+import csv
 
-print("world")
+def readData():
+    data = []
+
+    with open('sales.csv', 'r') as salesData:
+        spreadsheet = csv.DictReader(salesData)
+        for row in spreadsheet:
+            data.append(row)
+    return(data)
+
+
+# 2. Collect all of the sales from each month into a single list
+def listConverter():
+    data = readData()
+
+    salesList = []
+
+    for row in data:
+        sales = row['sales']
+        salesList.append(sales)
+    return(salesList)
+
+# 3. Output the total sales across all months
+def totalCount():
+    salesList = listConverter()
+    count = 0
+
+    for salesAmount in salesList:
+        count = count + int(salesAmount)
+    print(count)
+
+#################################       MAIN METHOD     #################################
+totalCount()
+

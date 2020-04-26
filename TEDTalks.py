a5d3bcfd2dbf5996f9fc6f90381cfd084b316fd6
bhags97 <bhagya28@hotmail.co.uk>
2020-04-26 18:13:42 +0100

    Added comments & functions

--- TEDTalks.py
+++ TEDTalks.py
@@ -2,6 +2,7 @@ import statistics
 import math
 import csv
 
+# Read the data from csv file
 def readData():
     data = []
 
@@ -11,30 +12,59 @@ def readData():
             data.append(row)
     return(data)
 
-
+# Make a list of the durations for each talk
 def listConverter():
     data = readData()
 
     durationList = []
 
     for row in data:
-        sales = row['duration']
-        durationList.append(sales)
+        duration = row['duration']
+        durationList.append(duration)
     return(durationList)
 
-def meanData():
+# Convert the list of string durations into int durations
+def intData():
     intDuration = []
     durationList = listConverter()
 
     for duration in durationList:
         intDuration.append(int(duration))
-    print(intDuration)
+    return(intDuration)
+
+
+# Calculate the mean duration
+def meanData():
+
+    intDuration = intData()
 
     mean = statistics.mean(intDuration)
     meanMinutes = round(mean/60, 1)
     print(f"Optimum duration is {meanMinutes} mintues")
 
+#################################       MAIN METHOD     #################################
 meanData()
+
+# Make a list of the tags for each talk
+def listTags():
+    data = readData()
+
+    tagsList = []
+
+    for row in data:
+        tags = row['tags']
+        tagsList.append(tags)
+    return(tagsList)
+
+# Make a dictionary of comments & tags as key value pairs
+def dictMaker():
+    intDuration = intData()
+    tagsList = listTags()
+
+    commentsTags = dict(zip(intDuration, tagsList))
+    print(commentsTags[1286])
+
+dictMaker()
 
 
 

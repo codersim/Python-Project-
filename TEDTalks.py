bfd5dbf3916c3c3628bbfb9cffcfbd01b07039dc
bhags97 <bhagya28@hotmail.co.uk>
2020-04-27 21:05:05 +0100

    Five Stats Analysis Methods

--- TEDTalks.py
+++ TEDTalks.py
@@ -88,5 +88,86 @@ def dictMaker():
 
 dictMaker()
 
+#################################       MAIN METHOD     #################################
+# Find which topics are the most popular
+from collections import Counter     #Collections is a module which contains the 'counter' tool used to provide tallied of items
+tagsList = listTags()
+most_common_topics = [word for word, word_count in Counter(tagsList).most_common(3)]
+print(f"The most popular themes are {most_common_topics}")
 
+
+# Make list of speakers
+def list_of_speakers():
+    data = readData()
+
+    speakerList = []
+
+    for row in data:
+        speaker = row['main_speaker']
+        speakerList.append(speaker)
+    return(speakerList)
+
+# Make list of URLs
+def list_of_URLs():
+    data = readData()
+
+    urlList = []
+
+    for row in data:
+        url = row['url']
+        urlList.append(url)
+    return(urlList)
+
+#################################       MAIN METHOD     #################################
+# Make a dictionary of speakers & urls as key value pairs
+def choose_your_talk():
+    speakerList = list_of_speakers()
+    urlList = list_of_URLs()
+    chosen_speaker = input("Which speaker do you want to listen to?")
+
+    speakerURL = dict(zip(speakerList, urlList))
+    print(speakerURL[chosen_speaker])
+
+choose_your_talk()
+
+
+# Make list of view numbers
+def list_of_views():
+    data = readData()
+
+    viewsList = []
+
+    for row in data:
+        views = row['views']
+        viewsList.append(views)
+    return(viewsList)
+
+
+# Make a dictionary of number of views & urls as key value pairs - note could have got index of comment and used this to find URL
+def most_viewed_talks():
+    viewsList = list_of_views()
+    urlList = list_of_URLs()
+    viewsURL = dict(zip(viewsList, urlList))
+
+    chosen_rank = input("Which video do you want to see from the top three rankings? Please enter first, second or third")
+
+    if chosen_rank == 'first':
+        high = max(viewsList)
+        print(viewsURL[high])
+    elif chosen_rank == 'second':
+        high = max(viewsList)
+        viewsList.remove(high)
+        second = max(viewsList)
+        print(viewsURL[second])
+    elif chosen_rank == 'third':
+        high = max(viewsList)
+        viewsList.remove(high)
+        second = max(viewsList)
+        viewsList.remove(second)
+        third = max(viewsList)
+        print(viewsURL[third])
+    else:
+        print("Error. Was expecting first, second or third")
+
+most_viewed_talks()
 
